119ec6e7e358630a9f00ea05bac640514145a9ad
Camiel Doorenweerd <c.doorenweerd@gmail.com>
2016-05-30 14:45:02 +0200

    fixed folder owner

--- miniconda_ete3_partitionfinder.sh
+++ miniconda_ete3_partitionfinder.sh
@@ -6,12 +6,13 @@
 # miniconda, partitionfinder dependencies
 cd /usr/local/
 sudo wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-bash Miniconda2-latest-Linux-x86_64.sh -b -p /usr/local/Miniconda
+sudo bash Miniconda2-latest-Linux-x86_64.sh -b -p /usr/local/Miniconda
 sudo sed -r -i 's/.{6}//' /etc/environment
-sudo sed -i 's|^|PATH="/home/ubuntu/Miniconda/bin:|' /etc/environment
+sudo sed -i 's|^|PATH="/usr/local/Miniconda/bin:|' /etc/environment
 sudo rm /usr/local/Miniconda2-latest-Linux-x86_64.sh
 cd
-export PATH="/home/ubuntu/Miniconda/bin:$PATH"
+export PATH="/usr/local/Miniconda/bin:$PATH"
+sudo chown ubuntu:root /usr/local/Miniconda -R
 conda install --yes -c blaze scipy pyparsing pandas pytables scikit-learn
 
 # ete3
@@ -24,3 +25,4 @@ cd partitionfinder
 sudo sed -i '1s|^|#!/usr/bin/env python\n|' /usr/local/partitionfinder/PartitionFinder.py
 sudo chmod +x /usr/local/partitionfinder/PartitionFinder.py
 sudo ln -s /usr/local/partitionfinder/PartitionFinder.py /usr/bin/partitionfinder
+
