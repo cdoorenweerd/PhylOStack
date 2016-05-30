67928210d2588912cb7dbe8f6439f0a83217ad1e
Camiel Doorenweerd <c.doorenweerd@gmail.com>
2016-05-30 14:10:24 +0200

    fixed missing dependencies

--- bamm.sh
+++ bamm.sh
@@ -3,16 +3,13 @@
 # Silent installation script for BAMM v2.5 on Linux Ubuntu 14.04
 # BAMM global executable: bamm
 
-# STDOUT log
-touch /home/ubuntu/BAMM_installation.log
-
 # dependencies
-sudo apt-get -y update >> /home/ubuntu/BAMM.log
-sudo apt-get -y install cmake >> /home/ubuntu/BAMM.log
+sudo apt-get -y update
+sudo apt-get -y install cmake build-essential
 
 # bamm
 cd /usr/local/
-sudo git clone https://github.com/macroevolution/bamm.git >> /home/ubuntu/BAMM.log
+sudo git clone https://github.com/macroevolution/bamm.git
 cd bamm
 sudo mkdir build
 cd build
