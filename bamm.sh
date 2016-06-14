1fdcd9b8af3e6abdc48455c8dbc984921c06e471
Camiel Doorenweerd <c.doorenweerd@gmail.com>
2016-06-14 13:42:29 +0200

    bamm build requires g++4.9 as C++11 compiler

--- bamm.sh
+++ bamm.sh
@@ -4,8 +4,11 @@
 # BAMM global executable: bamm
 
 # dependencies
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -y update
-sudo apt-get -y install cmake build-essential
+sudo apt-get -y install cmake build-essential gcc-4.9 g++-4.9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
+
 
 # bamm
 cd /usr/local/
