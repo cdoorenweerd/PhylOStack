41d3cc2d6c401b799f3e2c946bd6faf1e62f8126
Camiel Doorenweerd <c.doorenweerd@gmail.com>
2016-07-11 10:23:51 +0200

    initial commit

---
+++ install_all.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# This simple script will execute all the installation scripts
+
+bash /usr/local/PhylOStack/bamm.sh
+bash /usr/local/PhylOStack/beast_beagle_phyml.sh
+bash /usr/local/PhylOStack/exabayes.sh
+bash /usr/local/PhylOStack/mafft.sh
+bash /usr/local/PhylOStack/miniconda_ete3_partitionfinder.sh
+bash /usr/local/PhylOStack/p4.sh
+bash /usr/local/PhylOStack/raxml_rnr.sh
+bash /usr/local/PhylOStack/r_dendextend_bammtools.sh
