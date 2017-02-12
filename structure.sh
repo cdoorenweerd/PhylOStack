fb48e0ea8107a39bd95fbb879898003b063ee3a6
Camiel Doorenweerd <c.doorenweerd@gmail.com>
2017-02-12 15:09:01 +0100

    full paths

--- structure.sh
+++ structure.sh
@@ -3,10 +3,10 @@
 # Installation script for Structure 2.3.4 (Jul 2012). Compiled from source code because packages are built for 32 bit only. Executable: structure
 
 cd /usr/local
-mkdir structure
-cd structure
+sudo mkdir structure
+cd /usr/local/structure
 sudo wget http://pritchardlab.stanford.edu/structure_software/release_versions/v2.3.4/structure_kernel_source.tar.gz
 sudo tar -xzvf structure_kernel_source.tar.gz
-cd structure_kernel_src
+cd /usr/local/structure/structure_kernel_src
 sudo make
 sudo ln -s /usr/local/structure/structure_kernel_src/structure /usr/bin/structure
