2c57c13486583ff733b7405fb6ad0c77df99b2e4
Camiel Doorenweerd <c.doorenweerd@gmail.com>
2017-02-12 15:05:43 +0100

    extract files

--- structure.sh
+++ structure.sh
@@ -6,6 +6,7 @@ cd /usr/local
 mkdir structure
 cd structure
 sudo wget http://pritchardlab.stanford.edu/structure_software/release_versions/v2.3.4/structure_kernel_source.tar.gz
+sudo tar -xzvf structure_kernel_source.tar.gz
 cd structure_kernel_src
 sudo make
 sudo ln -s /usr/local/structure/structure_kernel_src/structure /usr/bin/structure
