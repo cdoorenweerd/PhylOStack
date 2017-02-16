a2e7ae4ff8d1b79c53c35e9abfb44583744e232a
Camiel Doorenweerd <c.doorenweerd@gmail.com>
2017-02-16 11:33:55 +0100

    added gnu parallel

--- structure.sh
+++ structure.sh
@@ -2,6 +2,7 @@
 #
 # Installation script for Structure 2.3.4 (Jul 2012). Compiled from source code because packages are built for 32 bit only. Executable: structure
 
+# download Structure and compile from source
 cd /usr/local
 sudo mkdir structure
 cd /usr/local/structure
@@ -10,3 +11,7 @@ sudo tar -xzvf structure_kernel_source.tar.gz
 cd /usr/local/structure/structure_kernel_src
 sudo make
 sudo ln -s /usr/local/structure/structure_kernel_src/structure /usr/bin/structure
+
+# install GNU parallel
+sudo apt-get update
+sudo apt-get install parallel
