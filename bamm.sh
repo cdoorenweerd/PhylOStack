c50e1fc73e4343872a7eab37698d9d1a50c52267
cdoorenweerd <cdoorenweerd@users.noreply.github.com>
2017-04-29 11:35:36 -1000

    made gcc6 default

--- bamm.sh
+++ bamm.sh
@@ -6,7 +6,8 @@
 # dependencies
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -y update
-sudo apt-get -y install cmake build-essential g++-6
+sudo apt-get -y install cmake build-essential gcc-6 g++-6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 
 
 # bamm
