26199207736d557de6ec4d32802b0862cdea02c5
cdoorenweerd <cdoorenweerd@users.noreply.github.com>
2017-04-29 09:22:07 -1000

    temp do not install

--- exabayes.sh
+++ exabayes.sh
@@ -7,15 +7,15 @@
 
 # dependencies
 sudo apt-get -y update
-sudo apt-get -y install git htop build-essential autoconf openmpi-bin libopenmpi-dev libmpich2-dev mpich2 subversion libtool pkg-config openjdk-6-jdk
+sudo apt-get -y install git htop build-essential autoconf autogen openmpi-bin libopenmpi-dev libmpich2-dev mpich2 subversion libtool pkg-config openjdk-6-jdk
 
 # exabayes
 cd /usr/local/
 sudo wget http://sco.h-its.org/exelixis/resource/download/software/exabayes-1.5.tar.gz
 sudo tar -xzf exabayes-1.5.tar.gz -C /usr/local/
-sudo chmod 777 /usr/local/exabayes-1.5/
-cd /usr/local/exabayes-1.5/
-./configure --enable-mpi && make -j 4
-sudo ln -s /usr/local/exabayes-1.5/exabayes /usr/bin/exabayes
-sudo ln -s /usr/local/exabayes-1.5/consense /usr/bin/consense
-sudo /sbin/ldconfig -v
+# sudo chmod 777 /usr/local/exabayes-1.5/
+# cd /usr/local/exabayes-1.5/
+# ./configure --enable-mpi && make -j
+# sudo ln -s /usr/local/exabayes-1.5/exabayes /usr/bin/exabayes
+# sudo ln -s /usr/local/exabayes-1.5/consense /usr/bin/consense
+# sudo /sbin/ldconfig -v
