e122be3f6f1afde91df0ef8cbbefb62b87fccf1c
cdoorenweerd <cdoorenweerd@users.noreply.github.com>
2017-05-04 11:10:50 -1000

    output to screen and log

--- install_all.sh
+++ install_all.sh
@@ -5,51 +5,51 @@
 # prevent 'unknown host' warning messages
 sudo sed -ie "1 s/$/ $(hostname)/" /etc/hosts
 
-echo "PHYLOSTACK INSTALLATION LOG" > /home/ubuntu/PhylOStack_installation.log
+echo PHYLOSTACK INSTALLATION LOG $(date) > /home/ubuntu/PhylOStack_installation.log
 
 echo "###############################################################" > /home/ubuntu/PhylOStack_installation.log
 echo "###################### BAMM installation ######################" > /home/ubuntu/PhylOStack_installation.log
 echo "###############################################################" > /home/ubuntu/PhylOStack_installation.log
-bash /usr/local/PhylOStack/bamm.sh
+bash /usr/local/PhylOStack/bamm.sh | tee /home/ubuntu/PhylOStack_installation.log
 
 echo "##########################################################################" > /home/ubuntu/PhylOStack_installation.log
 echo "###################### Beast and PhyML installation ######################" > /home/ubuntu/PhylOStack_installation.log
 echo "##########################################################################" > /home/ubuntu/PhylOStack_installation.log
-bash /usr/local/PhylOStack/beast_beagle_phyml.sh
+bash /usr/local/PhylOStack/beast_beagle_phyml.sh | tee /home/ubuntu/PhylOStack_installation.log
 
 echo "###################################################################" > /home/ubuntu/PhylOStack_installation.log
 echo "###################### Exabayes installation ######################" > /home/ubuntu/PhylOStack_installation.log
 echo "###################################################################" > /home/ubuntu/PhylOStack_installation.log
-bash /usr/local/PhylOStack/exabayes.sh
+bash /usr/local/PhylOStack/exabayes.sh | tee /home/ubuntu/PhylOStack_installation.log
 
 echo "################################################################" > /home/ubuntu/PhylOStack_installation.log
 echo "###################### MAFFT installation ######################" > /home/ubuntu/PhylOStack_installation.log
 echo "################################################################" > /home/ubuntu/PhylOStack_installation.log
-bash /usr/local/PhylOStack/mafft.sh
+bash /usr/local/PhylOStack/mafft.sh | tee /home/ubuntu/PhylOStack_installation.log
 
 echo "##############################################################################################" > /home/ubuntu/PhylOStack_installation.log
 echo "###################### Miniconda, ETE3 and PartitionFinder installation ######################" > /home/ubuntu/PhylOStack_installation.log
 echo "##############################################################################################" > /home/ubuntu/PhylOStack_installation.log
-bash /usr/local/PhylOStack/miniconda_ete3_partitionfinder.sh
+bash /usr/local/PhylOStack/miniconda_ete3_partitionfinder.sh | tee /home/ubuntu/PhylOStack_installation.log
 
 echo "#############################################################" > /home/ubuntu/PhylOStack_installation.log
 echo "###################### P4 installation ######################" > /home/ubuntu/PhylOStack_installation.log
 echo "#############################################################" > /home/ubuntu/PhylOStack_installation.log
-bash /usr/local/PhylOStack/p4.sh
+bash /usr/local/PhylOStack/p4.sh | tee /home/ubuntu/PhylOStack_installation.log
 
 echo "###############################################################################" > /home/ubuntu/PhylOStack_installation.log
 echo "###################### RaxML and RogueNaRok installation ######################" > /home/ubuntu/PhylOStack_installation.log
 echo "###############################################################################" > /home/ubuntu/PhylOStack_installation.log
-bash /usr/local/PhylOStack/raxml_rnr.sh
+bash /usr/local/PhylOStack/raxml_rnr.sh | tee /home/ubuntu/PhylOStack_installation.log
 
 echo "##########################################################################################" > /home/ubuntu/PhylOStack_installation.log
 echo "###################### R with Dendextend and BAMMtools installation ######################" > /home/ubuntu/PhylOStack_installation.log
 echo "##########################################################################################" > /home/ubuntu/PhylOStack_installation.log
-bash /usr/local/PhylOStack/r_dendextend_bammtools.sh
+bash /usr/local/PhylOStack/r_dendextend_bammtools.sh | tee /home/ubuntu/PhylOStack_installation.log
 
 echo "####################################################################" > /home/ubuntu/PhylOStack_installation.log
 echo "###################### Structure installation ######################" > /home/ubuntu/PhylOStack_installation.log
 echo "####################################################################" > /home/ubuntu/PhylOStack_installation.log
-bash /usr/local/PhylOStack/structure.sh
+bash /usr/local/PhylOStack/structure.sh | tee /home/ubuntu/PhylOStack_installation.log
 
 sudo reboot
